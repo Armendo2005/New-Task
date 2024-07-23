891eecb51b1583bf99bfdb6ede5644f1fdb064f2
Armendo <Armendo2005@gmail.com>
2024-07-23 03:18:03 +0300

    WorkSpace

--- linkDev-assessment/src/app/app.routes.ts
+++ linkDev-assessment/src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { MyRequestsComponent } from './features/my-requests/my-requests.componen
 import { DraftComponent } from './shared/components/draft/draft.component';
 import { EventsListingComponent } from './features/events/components/events-listing/events-listing.component';
 import { EventDetailsComponent } from './features/events/components/event-details/event-details.component';
+import { WorkspaceComponent } from './features/workspace/workspace.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/events', pathMatch: 'full' },
+  { path: '', redirectTo: '/workspace', pathMatch: 'full' },
   { path: 'events', component: EventsListingComponent },
   { path: 'events/:id', component: EventDetailsComponent }, 
   { path: 'my-requests', component: MyRequestsComponent },
+  { path: 'workspace', component: WorkspaceComponent },
   { path: 'draft', component: DraftComponent },
 
 ];
--- linkDev-assessment/src/app/features/workspace/workspace.component.ts
+++ linkDev-assessment/src/app/features/workspace/workspace.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-workspace',
   standalone: true,
-  imports: [],
+  imports: [NgFor, NgIf],
   templateUrl: './workspace.component.html',
   styleUrl: './workspace.component.scss'
 })
-export class WorkspaceComponent {
+export class WorkspaceComponent  implements OnInit {
+  inProgressRequestsCount: number = 0;
+  pendingTasksCount: number = 0;
+  latestRequests: any[] = [];
+  requestStatus: any[] = [];
 
+  private readonly dashboardUrl = 'https://api.npoint.io/287f15da7aaf2d316683/data';
+  private readonly statusUrl = 'https://api.npoint.io/0d1ae6c75a16d8886e30/data/requeststatus';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.loadDashboardData();
+    this.loadRequestStatus();
+  }
+
+  private loadDashboardData(): void {
+    this.http.get<any>(this.dashboardUrl).subscribe(data => {
+      console.log('Dashboard Data:', data); 
+      this.inProgressRequestsCount = data.inProgressRequestsCount || 0;
+      this.pendingTasksCount = data.pendingTasksCount || 0;
+      this.latestRequests = data.requestModel.slice(0, 5); // Get top 5 latest requests
+    });
+  }
+
+  private loadRequestStatus(): void {
+    this.http.get<any>(this.statusUrl).subscribe(data => {
+      this.requestStatus = data;
+      console.log('DATA STATUS:',this.requestStatus);
+    });
+  }
+
+  getStatusName(statusCode: string): string {
+    const status = this.requestStatus.find(s => s.statusId === statusCode);
+    return status ? status.statusName : 'Unknown';
+  }
 }
